7fedf09d7542cf963fd051e798147214d1e39475
yago-codev <yagomilano92@gmail.com>
2020-05-21 01:09:43 -0300

    chore: adicionando o jest

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  // definindo a pasta onde iremos fazer a cobertura de testes
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  // transpilando/convertendo os arquivos ts antes de rodarmos os testes
+  transform: {
+    '.*\\.ts$': 'ts-jest'
+  }
+}
